7c10bfec9ef50492cc342cedc6e313d7fb25e1ed
João Victor <victor_rs11@hotmail.com>
2020-08-02 12:30:18 -0300

    def fim

---
+++ pacote-download/python/ex105def.py
@@ -0,0 +1,25 @@
+def notas(*n, sit=False):
+    """
+    -> Função para analisar notas e situações de vários alunos.
+    :param n: uma ou mais notas dos alunos
+    :param sit: opcional False ou True
+    :return: dicionário com várias informações sobre a situação da turma
+    """
+    r = dict()
+    r['total'] = len(n)
+    r['maior'] = max(n)
+    r['menor'] = min(n)
+    r['media'] = sum(n) / len(n)
+    if sit:
+        if r['media'] >= 7:
+            r['situação'] = 'Boa'
+        elif r['media'] >= 5:
+            r['situação'] = 'Razoável'
+        else:
+            r['situação'] = 'Ruim'
+    return r
+
+
+# Programa principal
+resp = notas(5.5, 2.5, 8.5, sit=True)
+print(resp)
